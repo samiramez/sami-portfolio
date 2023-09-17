2f4b1f1d493f6e2977e8bd56e5c220093337737b
سامي اطنه لي <AD1008@iu.edu.jo>
2023-09-17 16:30:53 +0300

    changeing the avatar

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,36 @@
+// links
+
+import Link from "next/link";
+
+//icons {
+  import {RiYoutubeLine,
+    RiInstagramLine,
+  RiFacebookLine,
+  RiGithubFill,
+  RiBehanceLine} from 'react-icons/ri'
+
+  import {SiCodeforces} from 'react-icons/si'
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+  <div className="flex items-center gap-x-5 text-lg">
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiYoutubeLine/>
+      </Link>  
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiFacebookLine/>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiGithubFill/>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiInstagramLine/>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+      <SiCodeforces/>
+      </Link>
+  </div>
+  );
 };
 
 export default Socials;
--- components/Transition.js
+++ components/Transition.js
@@ -26,7 +26,6 @@ const Transition = () => {
       animate= "animate" 
       exit="exit"
       transition={{delay: 0.2, duration:0.6, ease:'easeInOut'}}>
-        1
         </motion.div>
       <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]' 
       variants={transisitionVariants} 
@@ -34,7 +33,6 @@ const Transition = () => {
       animate= "animate" 
       exit="exit"
       transition={{delay: 0.4, duration:0.6, ease:'easeInOut'}}>
-        1
         </motion.div>
       <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]' 
       variants={transisitionVariants} 
@@ -42,10 +40,7 @@ const Transition = () => {
       animate= "animate" 
       exit="exit"
       transition={{delay: 0.6, duration:0.6, ease:'easeInOut'}}>
-        1
         </motion.div>
-      
-
     </>
   );
 };
